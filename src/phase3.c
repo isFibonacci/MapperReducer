a502c344e68d00ef9cd02b713a350cd9dfb48953
isFibonacci <47167196+isFibonacci@users.noreply.github.com>
2019-12-05 00:26:13 -0600

    Update phase3.c

--- src/phase3.c
+++ src/phase3.c
@@ -9,13 +9,6 @@
 #include <dirent.h>
 #include <unistd.h>
 
-// You are free to use your own logic. The following points are just for getting started
-/* Reduce Function
-	1)	The reducer receives the list from 'm' mappers via pipes
-	2)	Ensure proper closing of pipe ends
-	3) 	Combine the lists to create a single list
-	4) 	Write the list to "ReducerResult.txt" in the current folder
-*/
 
 int reduce(int *temp) {
 	for (int i = 0; i < 26; i++){
